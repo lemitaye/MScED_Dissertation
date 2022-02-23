38ac6b824dbdee9f0c682037c690b27036a7f75e
Lemi Daba <tayelemi@gmail.com>
2022-02-23 17:28:18 +0000

    first draft

--- scripts/data_samples.R
+++ scripts/data_samples.R
@@ -1,8 +1,9 @@
+
 # Created on: February 22, 2022
 # DEPENDS ON: data_extract.R
 # Outputs:
-#   gt2_analysis_sample.csv
-#   gt3_analysis_sample.csv
+#   gt2_sample.csv
+#   gt3_sample.csv
 
 # This script loads the data produced by data_extract.R, processes it,
 # and produces cleaned data ready for analysis.
@@ -11,12 +12,13 @@ rm(list = ls())
 
 # Data Preparation ####
 
-# Load saved data 
+# Load saved data
 data <- read_csv("data/kids_data.csv")
 
 # convert all character columns into factors
-data <- data %>% 
-  mutate_if(is.character, as.factor)
+data <- data %>%
+  mutate_if(is.character, as.factor) %>% 
+  mutate(district = factor(district), municip = factor(municip))
 
 
 # Get the raw 2+ and 3+ samples
@@ -36,7 +38,7 @@ gt3_sample0 <- gt2_sample0 %>%
   filter(secondborn_age %>% between(6, 18))
 
 
-# This function enables us to get data frames with all the instruments 
+# This function enables us to get data frames with all the instruments
 get_parity <- function(tbl, n) {
   tbl <- tbl %>%
     select(moth_no, child_dob, birth_order, no_kids, child_sex) %>%
@@ -49,23 +51,22 @@ get_parity <- function(tbl, n) {
     ) %>%
     mutate(
       boy_1 = case_when(
-        child_sex_1 == 1 ~ 1,
+        child_sex_1 == "Male" ~ 1,
         TRUE ~ 0
       ),
       boy_2 = case_when(
-        child_sex_2 == 1 ~ 1,
+        child_sex_2 == "Male" ~ 1,
         TRUE ~ 0
       ),
       boy_12 = case_when(
-        (child_sex_1 == 1 & child_sex_2 == 1) ~ 1,
+        (child_sex_1 == "Male" & child_sex_2 == "Male") ~ 1,
         TRUE ~ 0
       ),
       girl_12 = case_when(
-        (child_sex_1 == 2 & child_sex_2 == 2) ~ 1,
+        (child_sex_1 == "Female" & child_sex_2 == "Female") ~ 1,
         TRUE ~ 0
       ),
       same_sex_12 = boy_12 + girl_12,
-
       twins_1 = case_when(
         (child_dob_1 == child_dob_2) ~ 1,
         TRUE ~ 0
@@ -74,22 +75,26 @@ get_parity <- function(tbl, n) {
         (child_dob_2 == child_dob_3) ~ 1,
         TRUE ~ 0
       )
-    ) 
+    )
 
   # The following is relevant for the 3+ sample:
   if (n == 4) {
     tbl <- tbl %>%
       mutate(
         boy_3 = case_when(
-          child_sex_3 == 1 ~ 1,
+          child_sex_3 == "Male" ~ 1,
           TRUE ~ 0
         ),
         boy_123 = case_when(
-          (child_sex_1 == 1 & child_sex_2 == 1 & child_sex_3 == 1) ~ 1,
+          (child_sex_1 == "Male" &
+            child_sex_2 == "Male" &
+            child_sex_3 == "Male") ~ 1,
           TRUE ~ 0
         ),
         girl_123 = case_when(
-          (child_sex_1 == 2 & child_sex_2 == 2 & child_sex_3 == 2) ~ 1,
+          (child_sex_1 == "Female" &
+            child_sex_2 == "Female" &
+            child_sex_3 == "Female") ~ 1,
           TRUE ~ 0
         ),
         same_sex_123 = boy_123 + girl_123,
@@ -99,17 +104,17 @@ get_parity <- function(tbl, n) {
         )
       )
   }
-  
-  tbl <- tbl %>% 
+
+  tbl <- tbl %>%
     select(-contains(c("child_sex", "child_dob")))
-  
+
   return(tbl)
 }
 
 
 # get all the instruments we need for the respective sample
-parity_gt2 <- get_parity(gt2_sample0, n = 3)  # for the 2+ sample
-parity_gt3 <- get_parity(gt3_sample0, n = 4)  # for the 3+ sample
+parity_gt2 <- get_parity(gt2_sample0, n = 3) # for the 2+ sample
+parity_gt3 <- get_parity(gt3_sample0, n = 4) # for the 3+ sample
 
 
 # Join with parity data frames and do some cleaning:
@@ -117,14 +122,16 @@ gt2_sample <- gt2_sample0 %>%
   left_join(parity_gt2, by = "moth_no") %>%
   filter(birth_order == 1) %>%
   # Filter out twins at first birth (and unrealistic obs.)
-  filter(!(twins_1 == 1), no_kids < 10) %>%
-  mutate(boy = case_when(child_sex == 1 ~ 1, child_sex == 2 ~ 0)) %>%
+  filter(!(twins_1 == 1), no_kids < 10, fath_age_year < 82) %>%
+  mutate( boy = case_when(
+    child_sex == "Male" ~ 1, 
+    child_sex == "Female" ~ 0) ) %>%
   select(
     moth_no:child_sex, boy, birth_order:twins_2, no_kids,
     everything(), -(firstborn_dob:secondborn_age)
-  ) %>% 
-  filter( !is.na(moth_dob), !is.na(fath_dob) ) 
-  
+  ) %>%
+  filter(!is.na(moth_dob), !is.na(fath_dob))
+
 
 gt3_sample <- gt3_sample0 %>%
   left_join(parity_gt3, by = "moth_no") %>%
@@ -134,7 +141,9 @@ gt3_sample <- gt3_sample0 %>%
     !(twins_1 == 1 | twins_2 == 1),
     no_kids < 10
   ) %>%
-  mutate(boy = case_when(child_sex == 1 ~ 1, child_sex == 2 ~ 0)) %>%
+  mutate( boy = case_when(
+    child_sex == "Male" ~ 1, 
+    child_sex == "Female" ~ 0)) %>%
   select(
     moth_no:child_sex, boy, birth_order:twins_2, twins_3, no_kids,
     everything(), -(firstborn_dob:secondborn_age)
@@ -145,20 +154,15 @@ gt3_sample <- gt3_sample0 %>%
 
 ## 2+ sample ####
 
-### Parents' education & background ####
-
-
-
-
 
 ### outcome variables ####
 
 # Dummy for private school attendance & child sex (factor)
 gt2_sample <- gt2_sample %>%
-  filter(child_private %in% c(1, 2)) %>%
+  filter(child_private %in% c("Private", "Public (government)")) %>%
   mutate(private_school = case_when(
-    child_private == 2 ~ 1, TRUE ~ 0
-  )) 
+    child_private == "Private" ~ 1, TRUE ~ 0
+  ))
 
 # constructing educational attainment variable
 gt2_sample <- gt2_sample %>%
@@ -173,6 +177,19 @@ gt2_sample <- gt2_sample %>%
   mutate(mean_educ_age_sex = mean(child_educ_gen)) %>%
   ungroup() %>%
   mutate(educ_attain = child_educ_gen / mean_educ_age_sex)
+
+
+### Parents' education & background ####
+
+gt2_sample <- gt2_sample %>% 
+  mutate(district = factor(district), municip = factor(municip)) %>% 
+  filter(!is.na(moth_inlf))
+
+
+
+
+
+
 
 
 
