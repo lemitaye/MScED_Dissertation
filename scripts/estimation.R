38411dcb611f256117eca20dd00c04dfee01cdde
Lemi Daba <tayelemi@gmail.com>
2022-02-23 16:54:21 +0000

    concise and elegant code with good results.

--- scripts/estimation.R
+++ scripts/estimation.R
@@ -4,6 +4,8 @@
 
 # This script runs the main regressions to be reported in the paper
 
+rm(list = ls())
+
 # Load saved data
 gt2_sample <- read_csv("data/gt2_sample.csv")
 gt3_sample <- read_csv("data/gt3_sample.csv")
@@ -90,43 +92,151 @@ linearHypothesis(m4_c, c("twins_2"))
 # 2SLS/IV regressions ####
 
 ## 2+ sample ####
-# (I think the OLS results are Wald estimates) 
-OLS_A1 <- lm(educ_attain ~ no_kids, data = gt2_analysis_sample)
-IV_A1 <- ivreg(educ_attain ~ no_kids | 
-                 twins_2, data = gt2_analysis_sample)
-IV_A2 <- ivreg(educ_attain ~ no_kids | 
-                 same_sex_12, data = gt2_analysis_sample)
-IV_A3 <- ivreg(educ_attain ~ no_kids | 
-                 boy_12 + girl_12, data = gt2_analysis_sample)
-IV_A4 <- ivreg(educ_attain ~ no_kids | 
-                 twins_2 + boy_12 + girl_12, data = gt2_analysis_sample)
-stargazer(OLS_A1, IV_A1, IV_A2, IV_A3, IV_A4, 
-          type = "text", keep.stat = c("n","rsq"))
 
-OLS_A2 <- lm(private_school ~ no_kids, data = gt2_analysis_sample)
-IV_A5 <- ivreg(private_school ~ no_kids | 
-                 twins_2, data = gt2_analysis_sample)
-IV_A6 <- ivreg(private_school ~ no_kids | 
-                 same_sex_12, data = gt2_analysis_sample)
-IV_A7 <- ivreg(private_school ~ no_kids | 
-                 boy_12 + girl_12, data = gt2_analysis_sample)
-IV_A8 <- ivreg(private_school ~ no_kids | 
-                 twins_2 + boy_12 + girl_12, data = gt2_analysis_sample)
-stargazer(OLS_A2, IV_A5, IV_A6, IV_A7, IV_A8, 
-          type = "text", keep.stat = c("n","rsq"))
+### Educational attainment ####
 
-# Tried both official and extended versions of female LFP
-OLS_A3 <- lm(moth_lfp_ext ~ no_kids, data = gt2_analysis_sample)
-IV_A9 <- ivreg(moth_lfp_ext ~ no_kids | 
-                 twins_2, data = gt2_analysis_sample)
-IV_A10 <- ivreg(moth_lfp_ext ~ no_kids | 
-                  same_sex_12, data = gt2_analysis_sample)
-IV_A11 <- ivreg(moth_lfp_ext ~ no_kids | 
-                  boy_12 + girl_12, data = gt2_analysis_sample)
-stargazer(OLS_A3, IV_A9, IV_A10, IV_A11, 
-          type = "text", keep.stat = c("n","rsq"))
+fOLS_A1 <- as.formula(
+  paste("educ_attain ~ no_kids + ", paste(covars, collapse = "+"))
+)
+
+fIV_A1 <- as.formula( # twins_2 instrument
+  paste(
+    "educ_attain ~ no_kids + ", paste(covars, collapse = "+"),
+    paste(" | twins_2 + ", paste(covars, collapse = "+"))
+  )
+)
+
+fIV_A2 <- as.formula( # same_sex instrument
+  paste(
+    "educ_attain ~ no_kids + ", paste(covars, collapse = "+"),
+    paste(" | same_sex_12 + ", paste(covars, collapse = "+") )
+  )
+)
+
+fIV_A3 <- as.formula( # same_sex instrument disaggregated
+  paste(
+    "educ_attain ~ no_kids + ", paste(covars, collapse = "+"),
+    paste(" | boy_12 + girl_12 + ", paste(covars, collapse = "+") )
+  )
+)
+
+fIV_A4 <- as.formula( # all instruments
+  paste(
+    "educ_attain ~ no_kids + ", paste(covars, collapse = "+"),
+    paste(" | twins_2 + boy_12 + girl_12 + ", paste(covars, collapse = "+") )
+  )
+)
+
+OLS_A1 <- lm(fOLS_A1, data = gt2_sample)
+IV_A1 <- ivreg(fIV_A1, data = gt2_sample)
+IV_A2 <- ivreg(fIV_A2, data = gt2_sample)
+IV_A3 <- ivreg(fIV_A3, data = gt2_sample)
+IV_A4 <- ivreg(fIV_A4, data = gt2_sample)
+
+stargazer(
+  OLS_A1, IV_A1, IV_A2, IV_A3, IV_A4,
+  keep = c("no_kids"),
+  type = "text",
+  keep.stat = c("n", "rsq")
+)
 
 
+### Private school attendance ####
+
+fOLS_A2 <- as.formula(
+  paste("private_school ~ no_kids + ", paste(covars, collapse = "+"))
+)
+
+fIV_A5 <- as.formula( # twins_2 instrument
+  paste(
+    "private_school ~ no_kids + ", paste(covars, collapse = "+"),
+    paste(" | twins_2 + ", paste(covars, collapse = "+") )
+  )
+)
+
+fIV_A6 <- as.formula( # same_sex instrument
+  paste(
+    "private_school ~ no_kids + ", paste(covars, collapse = "+"),
+    paste(" | same_sex_12 + ", paste(covars, collapse = "+") )
+  )
+)
+
+fIV_A7 <- as.formula( # same_sex instrument
+  paste(
+    "private_school ~ no_kids + ", paste(covars, collapse = "+"),
+    paste(" | boy_12 + girl_12 + ", paste(covars, collapse = "+") )
+  )
+)
+
+fIV_A8 <- as.formula( # same_sex instrument
+  paste(
+    "private_school ~ no_kids + ", paste(covars, collapse = "+"),
+    paste(" | twins_2 + boy_12 + girl_12 + ", paste(covars, collapse = "+") )
+  )
+)
+
+OLS_A2 <- lm(fOLS_A2, data = gt2_sample)
+IV_A5  <- ivreg(fIV_A5, data = gt2_sample)
+IV_A6  <- ivreg(fIV_A6, data = gt2_sample)
+IV_A7  <- ivreg(fIV_A7, data = gt2_sample)
+IV_A8  <- ivreg(fIV_A8, data = gt2_sample)
+
+stargazer(
+  OLS_A2, IV_A5, IV_A6, IV_A7, IV_A8,
+  keep = c("no_kids"),
+  type = "text",
+  keep.stat = c("n", "rsq")
+)
+
+
+### Mothers' LFP ####
+
+fOLS_A3 <- as.formula(
+  paste("moth_inlf ~ no_kids + ", paste(covars, collapse = "+"))
+)
+
+fIV_A9 <- as.formula( # twins_2 instrument
+  paste(
+    "moth_inlf ~ no_kids + ", paste(covars, collapse = "+"),
+    paste(" | twins_2 + ", paste(covars, collapse = "+") )
+  )
+)
+
+fIV_A10 <- as.formula( # same_sex instrument
+  paste(
+    "moth_inlf ~ no_kids + ", paste(covars, collapse = "+"),
+    paste(" | same_sex_12 + ", paste(covars, collapse = "+") )
+  )
+)
+
+fIV_A11 <- as.formula( # same_sex instrument
+  paste(
+    "moth_inlf ~ no_kids + ", paste(covars, collapse = "+"),
+    paste(" | boy_12 + girl_12 + ", paste(covars, collapse = "+") )
+  )
+)
+
+fIV_A12 <- as.formula( # same_sex instrument
+  paste(
+    "moth_inlf ~ no_kids + ", paste(covars, collapse = "+"),
+    paste(" | twins_2 + boy_12 + girl_12 + ", paste(covars, collapse = "+") )
+  )
+)
+
+OLS_A3 <- lm(fOLS_A3, data = gt2_sample)
+IV_A9  <- ivreg(fIV_A9, data = gt2_sample)
+IV_A10 <- ivreg(fIV_A10, data = gt2_sample)
+IV_A11 <- ivreg(fIV_A11, data = gt2_sample)
+IV_A12 <- ivreg(fIV_A12, data = gt2_sample)
+
+stargazer(
+  OLS_A3, IV_A9, IV_A10, IV_A11, IV_A12,
+  keep = c("no_kids"),
+  type = "text",
+  keep.stat = c("n", "rsq")
+)
+
+#####
 
 
 
