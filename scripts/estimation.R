b1a9c1235c42630ccf75dc8fbcd1eb2c0f938f62
Lemi Daba <tayelemi@gmail.com>
2022-02-24 03:49:57 +0000

    attempted a more concise code

--- scripts/estimation.R
+++ scripts/estimation.R
@@ -247,17 +247,59 @@ m7 <- lm(no_kids ~ twins_3, data = gt3_sample)
 stargazer(m5, m6, m7, type = "text", keep.stat = c("n","rsq"))
 
 # Educational attainment
-OLS_B1 <- lm(educ_attain ~ no_kids, data = gt3_sample)
-IV_B1 <- ivreg(educ_attain ~ no_kids | 
-                 twins_3, data = gt3_sample)
-IV_B2 <- ivreg(educ_attain ~ no_kids | 
-                 same_sex_123, data = gt3_sample)
-IV_B3 <- ivreg(educ_attain ~ no_kids | 
-                 boy_123 + girl_123, data = gt3_sample)
-IV_B4 <- ivreg(educ_attain ~ no_kids | 
-                 twins_3 + boy_123 + girl_123, data = gt3_sample)
-stargazer(OLS_B1, IV_B1, IV_B2, IV_B3, IV_B4, 
-          type = "text", keep.stat = c("n","rsq"))
+
+make_formula <- function(dep_var, instrument = 0) {
+  
+  covar1 <- c("child_age_month", "boy", "moth_age_year", "fath_inhh")
+  covar2 <- c("district", "moth_educ", "moth_pp_group", "moth_income")
+  covar <- paste( paste(covar1, collapse = "+"), "|",  
+                  paste(covar2, collapse = "+") )
+  
+  if (instrument == 0) {
+    f <- as.formula( paste( dep_var, " ~ no_kids + ", covar, 
+                            " | 0 | moth_no" ) )
+  } else {
+    f <- as.formula( paste( dep_var, " ~ ", covar, 
+                            " | (no_kids ~ ", instrument, ")", " | moth_no" ) )
+  }
+  
+  return(f)
+}
+
+fOLS_B1 <- make_formula("educ_attain")
+fIV_B1 <- make_formula("educ_attain", "twins_3")
+fIV_B2 <- make_formula("educ_attain", "same_sex_123")
+fIV_B3 <- make_formula("educ_attain", "boy_123 + girl_123")
+fIV_B4 <- make_formula("educ_attain", "twins_3 + boy_123 + girl_123")
+
+fOLS_B1 <- as.formula( paste("educ_attain ~ no_kids + ", covar, 
+                             " | 0 | moth_no") )
+
+fIV_B1 <- as.formula( paste("educ_attain ~ ", covar, 
+                  " | (no_kids ~ twins_3)", " | moth_no" ) )
+
+fIV_B2 <- as.formula( paste("educ_attain ~ ", covar, 
+                  " | (no_kids ~ same_sex_123)", " | moth_no") )
+
+fIV_B3 <- as.formula( paste("educ_attain ~ ", covar, 
+                  " | (no_kids ~ boy_123 + girl_123)", 
+                  " | moth_no") )
+
+fIV_B4 <- as.formula( paste("educ_attain ~ ", covar, 
+                  " | (no_kids ~ twins_3 + boy_123 + girl_123)", 
+                  " | moth_no") )
+
+OLS_B1 <- felm(fOLS_B1, data = gt3_sample)
+IV_B1 <- felm(fIV_B1, data = gt3_sample)
+IV_B2 <- felm(fIV_B2, data = gt3_sample)
+IV_B3 <- felm(fIV_B3, data = gt3_sample)
+IV_B4 <- felm(fIV_B4, data = gt3_sample)
+
+stargazer(
+  OLS_B1, IV_B1, IV_B2, IV_B3, IV_B4,
+  keep = c("no_kids"), 
+  type = "text", 
+  keep.stat = c("n","rsq"))
 
 # Private school attendance
 OLS_B2 <- lm(private_school ~ no_kids, data = gt3_sample)
@@ -271,6 +313,58 @@ IV_B8 <- ivreg(private_school ~ no_kids |
                  twins_3 + boy_123 + girl_123, data = gt3_sample)
 stargazer(OLS_B2, IV_B5, IV_B6, IV_B7, IV_B8, 
           type = "text", keep.stat = c("n","rsq"))
+
+
+# test
+
+covars <- c(
+  "child_age_month", "boy", "district", "moth_educ", "moth_pp_group", 
+  "moth_age_year", "moth_income", "fath_inhh"
+)
+
+fiv <- as.formula( # same_sex instrument
+  paste(
+    "moth_inlf ~ no_kids + ", paste(covars, collapse = "+"),
+    paste(" | boy_12 + girl_12 + ", paste(covars, collapse = "+") )
+  )
+)
+
+ivest <- felm(
+  educ_attain ~ no_kids + child_age_month + boy + moth_age_year + fath_inhh | 
+    district + moth_educ + moth_pp_group + moth_income  
+  data = gt2_sample
+  )
+
+
+stargazer(IV_A1, type = "text", keep = c("no_kids"), keep.stat = c("n","rsq"))
+stargazer(
+  summary(ivest), 
+  type = "text", 
+  keep = c("no_kids"), 
+  keep.stat = c("n","rsq")
+  )
+
+j <- felm(
+  educ_attain|private_school|moth_inlf ~ child_age_month + boy + 
+    moth_age_year + fath_inhh | district + moth_educ + moth_pp_group + 
+    moth_income | (no_kids ~ twins_2), 
+  data = gt2_sample
+)
+
+J_educ <- summary(j, lhs = "educ_attain", robust = TRUE)
+as(J_ed)
+
+stargazer(J_educ, 
+          lhs = "educ_attain",
+          type = "text", 
+          keep = c("no_kids"), 
+          keep.stat = c("n","rsq"))
+
+
+
+
+
+
 
 
 
