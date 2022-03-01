1aa00627abedd5bf7d07752b615291c41f067c3a
Lemi Daba <tayelemi@gmail.com>
2022-03-01 03:50:14 +0000

    tried several sub-sample estimations

--- scripts/estimation.R
+++ scripts/estimation.R
@@ -115,7 +115,7 @@ stargazer(
 
 ## 3+ sample ####
 
-# These are all clustere by mother's id
+# These are all clustered by mother's id
 fm_b1 <- make_formula_frst_stg("no_kids", "twins_3", clus = TRUE)
 fm_b2 <- make_formula_frst_stg("no_kids", "same_sex_123", clus = TRUE)
 fm_b3 <- make_formula_frst_stg("no_kids", "boy_123 + girl_123", clus = TRUE)
@@ -148,11 +148,11 @@ fIV_A2 <- make_formula_gt2("educ_attain", "same_sex_12")
 fIV_A3 <- make_formula_gt2("educ_attain", "boy_12 + girl_12")
 fIV_A4 <- make_formula_gt2("educ_attain", "twins_2 + boy_12 + girl_12")
 
-OLS_A1 <- felm(fOLS_A1, data = gt2_sample, subset = (child_age_year > 9))
-IV_A1 <- felm(fIV_A1, data = gt2_sample, subset = (child_age_year > 9))
-IV_A2 <- felm(fIV_A2, data = gt2_sample, subset = (child_age_year > 9))
-IV_A3 <- felm(fIV_A3, data = gt2_sample, subset = (child_age_year > 9))
-IV_A4 <- felm(fIV_A4, data = gt2_sample, subset = (child_age_year > 9))
+OLS_A1 <- felm(fOLS_A1, data = gt2_sample, subset = (child_age_year <= 14))
+IV_A1 <- felm(fIV_A1, data = gt2_sample, subset = (child_age_year <= 14))
+IV_A2 <- felm(fIV_A2, data = gt2_sample, subset = (child_age_year <= 14))
+IV_A3 <- felm(fIV_A3, data = gt2_sample, subset = (child_age_year <= 14))
+IV_A4 <- felm(fIV_A4, data = gt2_sample, subset = (child_age_year <= 14))
 
 stargazer(
   OLS_A1, IV_A1, IV_A2, IV_A3, IV_A4,
@@ -161,48 +161,6 @@ stargazer(
   keep.stat = c("n", "rsq")
 )
 
-
-### Private school attendance ####
-fOLS_A2 <- make_formula_gt2("private_school")
-fIV_A5 <- make_formula_gt2("private_school", "twins_2")
-fIV_A6 <- make_formula_gt2("private_school", "same_sex_12")
-fIV_A7 <- make_formula_gt2("private_school", "boy_12 + girl_12")
-fIV_A8 <- make_formula_gt2("private_school", "twins_2 + boy_12 + girl_12")
-
-OLS_A2 <- felm(fOLS_A2, data = gt2_sample, subset = (child_age_year > 9))
-IV_A5  <- felm(fIV_A5, data = gt2_sample, subset = (child_age_year > 9))
-IV_A6  <- felm(fIV_A6, data = gt2_sample, subset = (child_age_year > 9))
-IV_A7  <- felm(fIV_A7, data = gt2_sample, subset = (child_age_year > 9))
-IV_A8  <- felm(fIV_A8, data = gt2_sample, subset = (child_age_year > 9))
-
-stargazer(
-  OLS_A2, IV_A5, IV_A6, IV_A7, IV_A8,
-  keep = c("no_kids"),
-  type = "text",
-  keep.stat = c("n", "rsq")
-)
-
-
-### Mothers' LFP ####
-
-fOLS_A3 <- make_formula_gt2("moth_inlf")
-fIV_A9  <- make_formula_gt2("moth_inlf", "twins_2")
-fIV_A10 <- make_formula_gt2("moth_inlf", "same_sex_12")
-fIV_A11 <- make_formula_gt2("moth_inlf", "boy_12 + girl_12")
-fIV_A12 <- make_formula_gt2("moth_inlf", "twins_2 + boy_12 + girl_12")
-
-OLS_A3 <- felm(fOLS_A3, data = gt2_sample)
-IV_A9  <- felm(fIV_A9, data = gt2_sample)
-IV_A10 <- felm(fIV_A10, data = gt2_sample)
-IV_A11 <- felm(fIV_A11, data = gt2_sample)
-IV_A12 <- felm(fIV_A12, data = gt2_sample)
-
-stargazer(
-  OLS_A3, IV_A9, IV_A10, IV_A11, IV_A12,
-  keep = c("no_kids"),
-  type = "text",
-  keep.stat = c("n", "rsq")
-)
 
 ### Left Behind ####
 
@@ -224,6 +182,50 @@ stargazer(
   type = "text",
   keep.stat = c("n", "rsq")
 )
+
+
+### Private school attendance ####
+fOLS_A2 <- make_formula_gt2("private_school")
+fIV_A5 <- make_formula_gt2("private_school", "twins_2")
+fIV_A6 <- make_formula_gt2("private_school", "same_sex_12")
+fIV_A7 <- make_formula_gt2("private_school", "boy_12 + girl_12")
+fIV_A8 <- make_formula_gt2("private_school", "twins_2 + boy_12 + girl_12")
+
+OLS_A2 <- felm(fOLS_A2, data = gt2_sample, subset = (child_age_year > 14))
+IV_A5  <- felm(fIV_A5, data = gt2_sample, subset = (child_age_year > 14))
+IV_A6  <- felm(fIV_A6, data = gt2_sample, subset = (child_age_year > 14))
+IV_A7  <- felm(fIV_A7, data = gt2_sample, subset = (child_age_year > 14))
+IV_A8  <- felm(fIV_A8, data = gt2_sample, subset = (child_age_year > 14))
+
+stargazer(
+  OLS_A2, IV_A5, IV_A6, IV_A7, IV_A8,
+  keep = c("no_kids"),
+  type = "text",
+  keep.stat = c("n", "rsq")
+)
+
+
+### Mothers' LFP ####
+
+fOLS_A3 <- make_formula_gt2("moth_inlf")
+fIV_A9  <- make_formula_gt2("moth_inlf", "twins_2")
+fIV_A10 <- make_formula_gt2("moth_inlf", "same_sex_12")
+fIV_A11 <- make_formula_gt2("moth_inlf", "boy_12 + girl_12")
+fIV_A12 <- make_formula_gt2("moth_inlf", "twins_2 + boy_12 + girl_12")
+
+OLS_A3 <- felm(fOLS_A3, data = gt2_sample, )
+IV_A9  <- felm(fIV_A9, data = gt2_sample)
+IV_A10 <- felm(fIV_A10, data = gt2_sample)
+IV_A11 <- felm(fIV_A11, data = gt2_sample)
+IV_A12 <- felm(fIV_A12, data = gt2_sample)
+
+stargazer(
+  OLS_A3, IV_A9, IV_A10, IV_A11, IV_A12,
+  keep = c("no_kids"),
+  type = "text",
+  keep.stat = c("n", "rsq")
+)
+
 
 ## 3+ sample #####
 
@@ -248,6 +250,35 @@ stargazer(
   keep.stat = c("n","rsq")
   )
 
+
+### Left Behind ####
+fOLS_B4 <- make_formula_gt3("behind")
+fIV_B13 <- make_formula_gt3("behind", "twins_3")
+fIV_B14 <- make_formula_gt3("behind", "same_sex_123")
+fIV_B15 <- make_formula_gt3("behind", "boy_123 + girl_123")
+fIV_B16 <- make_formula_gt3("behind", "twins_3 + boy_123 + girl_123")
+
+OLS_B4 <- felm(fOLS_B4, data = gt3_sample, subset = (child_age_year > 9))
+IV_B13 <- felm(fIV_B13, data = gt3_sample, subset = (child_age_year > 9))
+IV_B14 <- felm(fIV_B14, data = gt3_sample, subset = (child_age_year > 9))
+IV_B15 <- felm(fIV_B15, data = gt3_sample, subset = (child_age_year > 9))
+IV_B16 <- felm(fIV_B16, data = gt3_sample, subset = (child_age_year > 9))
+
+stargazer(
+  OLS_B4, IV_B13, IV_B14, IV_B15, IV_B16, 
+  keep = c("no_kids"), 
+  type = "text", 
+  keep.stat = c("n","rsq")
+)
+
+
+# Think of ways of getting robust std. errors for the 2+ sample
+
+# Sub-sample analysis
+#  * population group (e.g., black and non-black)
+#  * mother's age (e.g., >30 and <= 30)
+
+
 ### Private school attendance ####
 fOLS_B2 <- make_formula_gt3("private_school")
 fIV_B5  <- make_formula_gt3("private_school", "twins_3")
@@ -255,11 +286,16 @@ fIV_B6  <- make_formula_gt3("private_school", "same_sex_123")
 fIV_B7  <- make_formula_gt3("private_school", "boy_123 + girl_123")
 fIV_B8  <- make_formula_gt3("private_school", "twins_3 + boy_123 + girl_123")
 
-OLS_B2 <- felm(fOLS_B2, data = gt3_sample)
-IV_B5  <- felm(fIV_B5, data = gt3_sample)
-IV_B6  <- felm(fIV_B6, data = gt3_sample)
-IV_B7  <- felm(fIV_B7, data = gt3_sample)
-IV_B8  <- felm(fIV_B8, data = gt3_sample)
+OLS_B2 <- felm(fOLS_B2, data = gt3_sample, 
+               subset = (child_age_year > 9) & (moth_pp_group != "Black African"))
+IV_B5  <- felm(fIV_B5, data = gt3_sample, 
+               subset = (child_age_year > 9) & (moth_pp_group != "Black African"))
+IV_B6  <- felm(fIV_B6, data = gt3_sample, 
+               subset = (child_age_year > 9) & (moth_pp_group != "Black African"))
+IV_B7  <- felm(fIV_B7, data = gt3_sample, 
+               subset = (child_age_year > 9) & (moth_pp_group != "Black African"))
+IV_B8  <- felm(fIV_B8, data = gt3_sample, 
+               subset = (child_age_year > 9) & (moth_pp_group != "Black African"))
 
 stargazer(
   OLS_B2, IV_B5, IV_B6, IV_B7, IV_B8, 
@@ -288,32 +324,98 @@ stargazer(
   keep.stat = c("n","rsq")
 )
 
-### Left Behind ####
-fOLS_B4 <- make_formula_gt3("behind")
-fIV_B13 <- make_formula_gt3("behind", "twins_3")
-fIV_B14 <- make_formula_gt3("behind", "same_sex_123")
-fIV_B15 <- make_formula_gt3("behind", "boy_123 + girl_123")
-fIV_B16 <- make_formula_gt3("behind", "twins_3 + boy_123 + girl_123")
 
-OLS_B4 <- felm(fOLS_B4, data = gt3_sample, subset = (child_age_year > 9))
-IV_B13 <- felm(fIV_B13, data = gt3_sample, subset = (child_age_year > 9))
-IV_B14 <- felm(fIV_B14, data = gt3_sample, subset = (child_age_year > 9))
-IV_B15 <- felm(fIV_B15, data = gt3_sample, subset = (child_age_year > 9))
-IV_B16 <- felm(fIV_B16, data = gt3_sample, subset = (child_age_year > 9))
+## Sub-sample analysis ####
+
+### Educational attainment ####
+
+OLS_SS_B1 <- felm(fOLS_A1, data = gt2_sample,
+                subset = (child_age_year > 9) & (moth_pp_group == "Black African"))
+IV_SS_B1 <- felm(fIV_A1, data = gt2_sample, 
+                subset = (child_age_year > 9) & (moth_pp_group == "Black African"))
+IV_SS_B3 <- felm(fIV_A3, data = gt2_sample, 
+                subset = (child_age_year > 9) & (moth_pp_group == "Black African"))
+IV_SS_B4 <- felm(fIV_A4, data = gt2_sample, 
+                subset = (child_age_year > 9) & (moth_pp_group == "Black African"))
+
+OLS_SS_NB1 <- felm(fOLS_A1, data = gt2_sample,
+                 subset = (child_age_year > 9) & (moth_pp_group != "Black African"))
+IV_SS_NB1 <- felm(fIV_A1, data = gt2_sample, 
+                subset = (child_age_year > 9) & (moth_pp_group != "Black African"))
+IV_SS_NB3 <- felm(fIV_A3, data = gt2_sample, 
+                subset = (child_age_year > 9) & (moth_pp_group != "Black African"))
+IV_SS_NB4 <- felm(fIV_A4, data = gt2_sample, 
+                subset = (child_age_year > 9) & (moth_pp_group != "Black African"))
 
 stargazer(
-  OLS_B4, IV_B13, IV_B14, IV_B15, IV_B16, 
-  keep = c("no_kids"), 
-  type = "text", 
-  keep.stat = c("n","rsq")
+  OLS_SS_B1, IV_SS_B1, IV_SS_B3, IV_SS_B4, OLS_SS_NB1, IV_SS_NB3, IV_SS_NB4,  
+  keep = c("no_kids"),
+  type = "text",
+  keep.stat = c("n", "rsq")
 )
 
 
-# Think of ways of getting robust std. errors for the 2+ sample
+### Left Behind ####
 
-# Sub-sample analysis
-#  * population group (e.g., black and non-black)
-#  * mother's age (e.g., >30 and <= 30)
+OLS_SS_B2 <- felm(fOLS_A4, data = gt2_sample,
+                  subset = (child_age_year > 9) & (moth_pp_group == "Black African"))
+IV_SS_B5 <- felm(fIV_A13, data = gt2_sample, 
+                 subset = (child_age_year > 9) & (moth_pp_group == "Black African"))
+IV_SS_B6 <- felm(fIV_A15, data = gt2_sample, 
+               subset = (child_age_year > 9) & (moth_pp_group == "Black African"))
+IV_SS_B7 <- felm(fIV_A16, data = gt2_sample, 
+                 subset = (child_age_year > 9) & (moth_pp_group == "Black African"))
+
+OLS_SS_NB2 <- felm(fOLS_A4, data = gt2_sample,
+                   subset = (child_age_year > 9) & (moth_pp_group != "Black African"))
+IV_SS_NB5 <- felm(fIV_A13, data = gt2_sample, 
+                  subset = (child_age_year > 9) & (moth_pp_group != "Black African"))
+IV_SS_NB6 <- felm(fIV_A15, data = gt2_sample, 
+                  subset = (child_age_year > 9) & (moth_pp_group != "Black African"))
+IV_SS_NB7 <- felm(fIV_A16, data = gt2_sample, 
+                  subset = (child_age_year > 9) & (moth_pp_group != "Black African"))
+
+stargazer(
+  OLS_SS_B2, IV_SS_B5, IV_SS_B6, IV_SS_B7, OLS_SS_NB2, IV_SS_NB5, IV_SS_NB6, IV_SS_NB7,  
+  keep = c("no_kids"),
+  type = "text",
+  keep.stat = c("n", "rsq")
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
