ff2ab2d3c238e4ecfeecd05d9979ac29be1f75d0
Lemi Daba <tayelemi@gmail.com>
2022-03-18 17:01:56 +0000

    First draft of summary statistics

--- scripts/descriptives.R
+++ scripts/descriptives.R
@@ -220,91 +220,58 @@ data_model_ols %>%
     caption = "The red dots indicate coefficient estimate and the lines are 95% confidence intervals"
   )
 
-# Tables of Descriptive Statistics ####
+# Tables of Summary Statistics ####
 
 gt2_descript <- gt2_sample %>% 
   select(
-    no_kids,
-    twins_2,
-    same_sex_12,
-    boy_12,
-    girl_12,
-    male = boy,
-    child_age_year,
-    child_age_month,
-    child_educ,
-    educ_attain,
-    behind,
-    private_school,
-    moth_age_year,
-    moth_age_fstbr,
-    moth_pp_group,
-    moth_educ,
-    moth_marital,
-    moth_inlf,
-    fath_inhh,
-    province
+    no_kids, twins_2, same_sex_12, boy_12, girl_12, male = boy,
+    child_age_year, child_age_month, child_educ, educ_attain,
+    behind, private_school, moth_age_year, moth_age_fstbr,
+    moth_pp_group, moth_educ, moth_marital, moth_inlf, fath_inhh
     ) %>% dummy_cols(
-      c("moth_pp_group", "moth_educ", "moth_marital", "province")
+      c("moth_pp_group", "moth_educ", "moth_marital")
       ) %>% 
-  select(-c(moth_pp_group, moth_educ, province, moth_marital))
+  select(-c(moth_pp_group, moth_educ, moth_marital))
 
 gt3_descript <- gt3_sample %>% 
   filter(!is.na(moth_pp_group)) %>% 
   select(
-    no_kids,
-    twins_3,
-    same_sex_123,
-    boy_123,
-    girl_123,
-    male = boy,
-    child_age_year,
-    child_age_month,
-    child_educ,
-    educ_attain,
-    behind,
-    private_school,
-    moth_age_year,
-    moth_age_fstbr,
-    moth_pp_group,
-    moth_educ,
-    moth_marital,
-    moth_inlf,
-    fath_inhh,
-    province,
-    birth_order
+    no_kids, twins_3, same_sex_123, boy_123, girl_123, male = boy,
+    child_age_year, child_age_month, child_educ, educ_attain, behind,
+    private_school, moth_age_year, moth_age_fstbr, moth_pp_group,
+    moth_educ, moth_marital, moth_inlf, fath_inhh, birth_order
   ) 
 
 gt3_descript_frstbrn <- gt3_descript %>% 
   filter(birth_order == 1) %>% 
   dummy_cols(
-    c("moth_pp_group", "moth_educ", "moth_marital", "province")
+    c("moth_pp_group", "moth_educ", "moth_marital")
   ) %>% 
   select(
-    -c(moth_pp_group, moth_educ, province, moth_marital, birth_order)
+    -c(moth_pp_group, moth_educ, moth_marital, birth_order)
   )
 
 gt3_descript_both <- gt3_descript %>%
   dummy_cols(
-  c("moth_pp_group", "moth_educ", "moth_marital", "province")
+  c("moth_pp_group", "moth_educ", "moth_marital")
 ) %>% 
   select(
-    -c(moth_pp_group, moth_educ, province, moth_marital, birth_order)
+    -c(moth_pp_group, moth_educ, moth_marital, birth_order)
     )
 
 sd_rm_bin <- function(x) {
   
-  if ( min(x, na.rm = TRUE) == 0 && max(x, na.rm = TRUE) == 1 ) {
+  if ( min(x) == 0 && max(x) == 1 ) {
     res <- NA
   } else {
-    res <- sd(x, na.rm = TRUE)
+    res <- sd(x)
   }
 
   res
 }
 
 mean_2 <- gt2_descript %>% 
-  map_dbl(mean, na.rm = TRUE) %>% 
+  map_dbl(mean) %>% 
   round(3)
 
 sd_2 <- gt2_descript %>% 
@@ -312,7 +279,7 @@ sd_2 <- gt2_descript %>%
   round(3)
 
 mean_3_frstbrn <- gt3_descript_frstbrn %>% 
-  map_dbl(mean, na.rm = TRUE) %>% 
+  map_dbl(mean) %>% 
   round(3)
 
 sd_3_frstbrn <- gt3_descript_frstbrn %>% 
@@ -320,7 +287,7 @@ sd_3_frstbrn <- gt3_descript_frstbrn %>%
   round(3)
 
 mean_3_both <- gt3_descript_both %>% 
-  map_dbl(mean, na.rm = TRUE) %>% 
+  map_dbl(mean) %>% 
   round(3)
 
 sd_3_both <- gt3_descript_both %>% 
@@ -335,29 +302,71 @@ y <- cbind(mean_3_frstbrn, sd_3_frstbrn, mean_3_both, sd_3_both) %>%
   data.frame() %>% 
   rownames_to_column("variable")
 
-z <- full_join(x, y, by = "variable")
+z <- full_join(x, y, by = "variable") 
 z$empty0 <- NA
 z$empty1 <- NA
 z$empty2 <- NA
 z <- select(z, empty0, variable, mean_2, sd_2, empty1, mean_3_frstbrn,
             sd_3_frstbrn, empty2, mean_3_both, sd_3_both)
 
-cols <- pull(z, "variable")  
-cbind(cols) # to see the vector
-cols_arrg <- cols[
-  c(1, 6, 7, 8, 9, 2, 17, 3, 4, 5, 18, 19, 20, 10, 11, 12, 13, 14, 16, 15)
-]
+z <- z %>% 
+  mutate(
+    variable = str_remove(
+      variable, "moth_pp_group_|moth_educ_|moth_marital_"
+    ),
+    variable = str_replace_all(
+      variable,
+      c(
+        "no_kids" = "Number of Children",
+        "twins_2" = "Twins2",
+        "same_sex_12" = "SameSex12",
+        "boy_12" = "Boy12",
+        "girl_12" = "Girl12",
+        "male" = "Male",
+        "child_age_year" = "Age (Years)",
+        "child_age_month" = "Age (Months)",
+        "child_educ" = "Grade Achieved",
+        "educ_attain" = "Educational Attainment",
+        "behind" = "Left Behind",
+        "private_school" = "Private School",
+        "moth_age_year" = "Mother's Age (Years)",
+        "moth_age_fstbr" = "Age at First Birth",
+        "moth_inlf" = "Mother in the Labour Force",
+        "fath_inhh" = "Father in Household",
+        "Some primary" = "Some Primary",
+        "Completed primary" = "Completed Primary",
+        "Some secondary" = "Some Secondary",
+        "Living together" = "Living Together",
+        "Never married" = "Never Married",
+        "Widower/widow" = "Widower",
+        "twins_3" = "Twins3",
+        "same_sex_123" = "SameSex123",
+        "boy_123" = "Boy123",
+        "girl_123" = "Girl123"
+        )
+    )
+    )  
+  
+cols <- pull(z, "variable")
+cbind(cols) # to see in a vertical list
 
-# cols <- c(
-#   "no_kids", "male", "child_age_year", "child_age_month", "child_educ", "twins_2", 
-#   "twins_3", "same_sex_12", "boy_12", "girl_12", "same_sex_123", "boy_123", "girl_123",
-#   "moth_age_year", "moth_age_fstbr", "fath_in_hh", "educ_attain", "behind"
-# )
+cols_arrg <- c(
+  "Number of Children", "Male", "Age (Years)", "Age (Months)", "Twins2" ,"SameSex12",                 
+  "Boy12", "Girl12", "Twins3", "SameSex123", "Boy123", "Girl123", "Grade Achieved",
+  "Educational Attainment", "Left Behind", "Private School", "Mother's Age (Years)",
+  "Age at First Birth", "Mother in the Labour Force", "Father in Household",
+  "Black African", "Coloured", "White", "Indian or Asian", "Other", 
+  "No schooling", "Some Primary", "Completed Primary", "Some Secondary",
+  "Grade 12/Std 10", "Higher",  
+  "Never Married", "Married", "Living Together", "Separated", "Divorced", "Widower"
+)
 
 z$variable <- factor(z$variable, levels = cols_arrg)
 z <- z[order(z$variable), ]
+row.names(z) <- NULL  # reset the rownames
 
-names <- c("Number of Kids", "Male", "Age in years", "Age in months")
+# 
+# names <- c("Number of Kids", "Male", "Age in years", "Age in months")
 
 
 xtab <- xtable(z, caption = "Summary Statistics") 
@@ -365,64 +374,58 @@ xtab <- xtable(z, caption = "Summary Statistics")
 # align(xtab) <- "rllccp{0.5cm}cc" {rllrrlrrlrr}
 # digits(xtab) <- 3
 
+nobs_descr_wrap <- function(chr, n = 2, a = "c") {
+  
+  n <- as.character(n)
+  chr <- as.character(chr)
+  
+  paste0( "\\multicolumn{", n, "}{", a, "}{", chr, "}" )
+  
+}
+
+obs_descr <- c(
+  nobs_descr_wrap( comma( nrow(gt2_descript) ) ), 
+  nobs_descr_wrap( comma( nrow(gt3_descript_frstbrn) ) ), 
+  nobs_descr_wrap( comma( nrow(gt3_descript_both) ) )
+)
+
+obs_descr <- paste( paste(obs_descr, collapse = " & & "), "\\\\" )
+obs_descr <- paste( nobs_descr_wrap( " $ N $ ", a = "l" ), " & ", 
+                    obs_descr )
+
+comm <- paste0(" \n \\\\[-1.8ex] \\multicolumn{10}{l}",
+               "{\\scriptsize{The standard deviations for proportions is 
+               not presented.}} \n")
+
+
 addtorow <- list()
-addtorow$pos <- list(0, 0, 0, 0, 16, 21, 28, 34)
+addtorow$pos <- list(0, 0, 0, 0, 0, 0, 0, 20, 25, 31, 37, 37, 37, 37)
 addtorow$command <- c(
-  " & & \\multicolumn{2}{c}{2+ Sample} & & \\multicolumn{4}{c}{3+ Sample}  \\\\",
-  "\\cline{3-4}  \\cline{6-10}",
-  " & & \\multicolumn{2}{c}{Firstborns} & & \\multicolumn{2}{c}{Firstborns} & & \\multicolumn{2}{c}{Secondborns} \\\\",
+  " \\\\[-1.8ex]",
+  " & & \\multicolumn{2}{c}{2+ Sample} & & \\multicolumn{5}{c}{3+ Sample}  \\\\[0.2ex]",
+  "\\cline{3-4}  \\cline{6-10}  \\\\[-1.2ex]" ,
+  " & & & & & & & & \\multicolumn{2}{c}{First and} \\\\",
+  " & & \\multicolumn{2}{c}{Firstborns} & & \\multicolumn{2}{c}{Firstborns} & & \\multicolumn{2}{c}{Second Borns} \\\\",
+  "\\cline{3-4}  \\cline{6-7} \\cline{9-10}  \\\\[-1.2ex]" ,
   " & & \\multicolumn{1}{c}{Mean} & \\multicolumn{1}{c}{sd} & & \\multicolumn{1}{c}{Mean} & \\multicolumn{1}{c}{sd} & & 
   \\multicolumn{1}{c}{Mean} & \\multicolumn{1}{c}{sd} \\\\",
   "\\multicolumn{2}{l}{Mother's Population Group} &  &  &  & & & & \\\\",
   "\\multicolumn{2}{l}{Mother's Level of Education} &  &  &  & & & & \\\\",
   "\\multicolumn{2}{l}{Mother's Marital Status} &  &  &  & & & & \\\\",
-  "\\multicolumn{2}{l}{Province} &  &  &  & & & & \\\\"
+  " \\\\[-1.8ex] \\hline \\\\[-1.8ex] ",
+  obs_descr,
+  " \\bottomrule ",
+  comm
   )
-
-print(
-  xtab,   
-  add.to.row = addtorow,
-  include.rownames = FALSE,
-  include.colnames = FALSE,
-  booktabs = TRUE,
-  floating = FALSE,
-  tabular.environment = "longtable",
-  # floating.environment = "sidewaystable",
-  file = "D:/MSc_ED/Thesis/SA_2011_Census/outline/tables/table10.tex"
-)
-# Xtable
 
 display(xtab) <- c("s", "s", "s", "g", "g", "s", "g", "g", "s", "g", "g")
 digits(xtab) <- 4
 
-
-xtable(mtcars, align = xalign(mtcars), digits = xdigits(mtcars),
-       display = xdisplay(mtcars))
-
-
-# include birth order in the 3+ sample
-# table for first stage, descriptive stat. and main regressions
-# Foucs on first stage
-# marital status of the mother in regs.
-
-# Heterogeneity of First Stage ####
-
-gt2_sample %>% 
-  group_by(twins_2) %>% 
-  summarise(
-    n = n(),
-    mean_age_frstbr = mean(moth_age_fstbr_gen),
-    sd_age_frstbr = sd(moth_age_fstbr_gen)        
-    )
-
-ha1 <- lm(moth_age_fstbr ~ twins_2, data = gt2_sample)
-summary(ha1)
-
-ha2 <- lm(moth_age_fstbr ~ twins_3, data = gt3_sample, 
-          subset = birth_order == 1)
-summary(ha2)
-
-
+print(
+  xtab, add.to.row = addtorow,
+  include.rownames = FALSE, include.colnames = FALSE,
+  booktabs = TRUE, caption.placement = "top", hline.after = c(-1, 0),
+  file = "D:/MSc_ED/Thesis/SA_2011_Census/outline/tables/table10.tex" )
 
 
 
@@ -433,8 +436,8 @@ gt3_sample %>%
   group_by(twins_3) %>% 
   summarise(
     n = n(),
-    mean_age_frstbr = mean(moth_age_fstbr_gen, na.rm = TRUE),
-    sd_age_frstbr = sd(moth_age_fstbr_gen, na.rm = TRUE)        
+    mean_age_frstbr = mean(moth_age_fstbr_gen),
+    sd_age_frstbr = sd(moth_age_fstbr_gen)        
   )
 
 # Not that much robust in the 3+ sample
